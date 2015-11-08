181aee4b78724f108eeb63b037a2088e900603a9
EvgeneOskin <eoskin@crystalnix.com>
2015-11-09 00:28:09 +0600

    Add administrator models.
    Motivation: use it to authenticate access to admin page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :administrators
   namespace :admin do
     DashboardManifest::DASHBOARDS.each do |dashboard_resource|
       resources dashboard_resource
--- test/factories.rb
+++ test/factories.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
+  factory :administrator do
+  end
+
   factory :user do
     email { Faker::Internet.email }
     password 'password'
